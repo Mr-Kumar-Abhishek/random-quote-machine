b69cc0e94e212d64d4548018916c7d4e36d0b861
Abhishek Kumar <mr.kumar.abhishek@email.com>
2016-12-04 14:44:10 +0530

    Do not use JSONProxy by Aidan Feldman
    fix for issue #1

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
  $(document).ready(function() {
-     $.getJSON("https://jsonp.afeld.me/?url=http%3A%2F%2Fquotes.stormconsultancy.co.uk%2Frandom.json",function(json) {
-      $("#quote").text(json.quote);
-      $("#author").html(json.author);
-      $("#tweet_this").attr('href', 'http://www.twitter.com/intent/tweet?text=' + json.quote + ' -- ' + json.author); 
+     $.getJSON("http://quotes.stormconsultancy.co.uk/random.json?callback=?",function(data) {
+      $("#quote").text(data.quote);
+      $("#author").html(data.author);
+      $("#tweet_this").attr('href', 'http://www.twitter.com/intent/tweet?text=' + data.quote + ' -- ' + data.author); 
      });
  });
 
 $("#get_quote").click(function(){
-    $.getJSON("https://jsonp.afeld.me/?url=http%3A%2F%2Fquotes.stormconsultancy.co.uk%2Frandom.json",function(json) {
-      $("#quote").text(json.quote);
-      $("#author").html(json.author);
-      $("#tweet_this").attr('href', 'http://www.twitter.com/intent/tweet?text=' + json.quote + ' -- ' + json.author); 
+    $.getJSON("http://quotes.stormconsultancy.co.uk/random.json?callback=?",function(data) {
+      $("#quote").text(data.quote);
+      $("#author").html(data.author);
+      $("#tweet_this").attr('href', 'http://www.twitter.com/intent/tweet?text=' + data.quote + ' -- ' + data.author); 
      });
 });

83690308dd14e6d809d6d115bba0a45e7ab02f38
Abhishek Kumar <mr.kumar.abhishek@email.com>
2016-12-06 00:59:19 +0530

    minor refactor

--- js/index.js
+++ js/index.js
@@ -1,15 +1,12 @@
  $(document).ready(function() {
-     $.getJSON("http://quotes.stormconsultancy.co.uk/random.json?callback=?",function(data) {
-      $("#quote").text(data.quote);
-      $("#author").html(data.author);
-      $("#tweet_this").attr('href', 'http://www.twitter.com/intent/tweet?text=' + data.quote + ' -- ' + data.author); 
-     });
+	get_quote();
+	$('#get_quote').click(get_quote);
  });
 
-$("#get_quote").click(function(){
+function get_quote(){
     $.getJSON("http://quotes.stormconsultancy.co.uk/random.json?callback=?",function(data) {
       $("#quote").text(data.quote);
       $("#author").html(data.author);
       $("#tweet_this").attr('href', 'http://www.twitter.com/intent/tweet?text=' + data.quote + ' -- ' + data.author); 
      });
-});
+}

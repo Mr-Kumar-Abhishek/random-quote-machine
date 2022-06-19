bac4771d307daba234099f67e8979f29b9de8c92
Abhishek Kumar <mr.kumar.abhishek@email.com>
2022-06-19 12:25:02 +0530

    change api

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@
  });
 
 function get_quote(){
-    $.getJSON("http://quotes.stormconsultancy.co.uk/random.json?callback=?",function(data) {
-      $("#text").text(data.quote);
+    $.getJSON("https://programming-quotes-api.herokuapp.com/Quotes/random",function(data) {
+      $("#text").text(data.en);
       $("#author").html(data.author);
       $("#tweet-quote").attr('href', 'http://www.twitter.com/intent/tweet?text=' + data.quote + ' -- ' + data.author); 
      });

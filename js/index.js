2375bc8ca73f977476e21464fc2a0c3db73ec9c7
Abhishek Kumar <mr.kumar.abhishek@email.com>
2022-11-25 14:33:49 +0530

    tweet bug fix

--- js/index.js
+++ js/index.js
@@ -7,6 +7,6 @@ function get_quote(){
     $.getJSON("https://programming-quotes-api.herokuapp.com/Quotes/random",function(data) {
       $("#text").text(data.en);
       $("#author").html(data.author);
-      $("#tweet-quote").attr('href', 'http://www.twitter.com/intent/tweet?text=' + data.quote + ' -- ' + data.author); 
+      $("#tweet-quote").attr('href', 'http://www.twitter.com/intent/tweet?text=' + data.en + ' -- ' + data.author); 
      });
 }
